258a4d6f449bd4396ceeed0af24192ac33fdc8e6
code-money <825018478@qq.com>
2022-06-27 18:17:44 +0800

    提交

--- study01/09_IOAndProperties/src/cn/zyz/demo01/OutputStream/Demo01OutputStream.java
+++ study01/09_IOAndProperties/src/cn/zyz/demo01/OutputStream/Demo01OutputStream.java
@@ -1,5 +1,8 @@
 package cn.zyz.demo01.OutputStream;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 
 /**
@@ -9,7 +12,13 @@ import java.io.OutputStream;
  **/
 
 public class Demo01OutputStream {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(new File("a.txt"));
+        fileOutputStream.write(97);
+        fileOutputStream.write(98);
 
+        fileOutputStream.close();
+    }
 }
 
 
